5cf3d04d2ed8d91247e1273278effccd95a4d535
Peter Swire <swirepe@gmail.com>
2012-04-28 23:39:45 -0400

    added a windows binary and a readme

--- each.py
+++ each.py
@@ -1,36 +1,35 @@
 import sys
 from eachParser import parse
 from eachRunner import runEach
-if __name__ != "__main__":
-	sys.exit(0)
 
-args = sys.argv[1:]
+if __name__ == "__main__":
+	args = sys.argv[1:]
 
-if len(args) <= 0:
-	print "each: an iterator for files"
-	print "Peter Swire - swirepe.com"
-	print "    "
-	print "General syntax:"
-	print "   each <name> in <list>\tassign, one by one, the values in <list> to <name>"
-	print "   ::\tThe command to run with in iteration.  It is run in series."
-	print "   ++\tThe command to run with in iteration.  It is run in parallel."
-	print "     \tOnly one of :: or ++ is used by this program."  
-	print "     \tAny after the first are assumed to be arguments for an external command"
-	print "      "
-	print "Example usage:"
-	print "   With an implied variable $"
-	print "      each *.zip :: unzip $"
-	print "    "
-	print "   With a named variable"
-	print "       each picture in *.jpg :: do_something picture"
-	print "    "
-	print "   With more than one type of file"
-	print "       each picture in *.jpg *.png :: do_something picture"
-	print "   "
-	print "   With things run in parallel instead of series"
-	print "       each picture in *.jpg *.png ++ do_something picture"
-	sys.exit(0)
+	if len(args) <= 0:
+		print "each: an iterator for files"
+		print "Peter Swire - swirepe.com"
+		print "    "
+		print "General syntax:"
+		print "   each <name> in <list>\tassign, one by one, the values in <list> to <name>"
+		print "   ::\tThe command to run with in iteration.  It is run in series."
+		print "   ++\tThe command to run with in iteration.  It is run in parallel."
+		print "     \tOnly one of :: or ++ is used by this program."  
+		print "     \tAny after the first are assumed to be arguments for an external command"
+		print "      "
+		print "Example usage:"
+		print "   With an implied variable $"
+		print "      each *.zip :: unzip $"
+		print "    "
+		print "   With a named variable"
+		print "       each picture in *.jpg :: do_something picture"
+		print "    "
+		print "   With more than one type of file"
+		print "       each picture in *.jpg *.png :: do_something picture"
+		print "   "
+		print "   With things run in parallel instead of series"
+		print "       each picture in *.jpg *.png ++ do_something picture"
+		sys.exit(0)
 
 
-parsedTuple = parse(args)
-runEach(parsedTuple)
+	parsedTuple = parse(args)
+	runEach(parsedTuple)
---
+++ readme.md
@@ -0,0 +1,45 @@
+# Each
+
+A small tool for iterating over files.
+
+Peter Swire, swirepe.com
+
+## What does it do?
+
+It allows you to specify a name for a file as you iterate through them.  Think of it as a foreach loop.
+
+## What do you use it for?
+
+For those times when you have a command that can't take in a glob.  It can run commands in series or parallel.
+
+    each markdownFile in *.md :: convertToHTML markdownFile
+
+
+## Help
+
+	 each: an iterator for files
+	 Peter Swire - swirepe.com
+	     
+	 General syntax:
+	    each <name> in <list> :: <command>
+	        assign, one by one, the values in <list> to <name>
+	        The values of <name> are replaced in <command>
+	    ::
+	        Run in series.
+	    ++
+	        Run in parallel, each in its own process.
+	        Only one of :: or ++ is used by this program.  
+	        Any after the first are assumed to be arguments for an external command
+	       
+	 Example usage:
+	    With an implied variable $
+	       each *.zip :: unzip $
+	     
+	    With a named variable
+	        each picture in *.jpg :: do_something picture
+	     
+	    With more than one type of file
+	        each picture in *.jpg *.png :: do_something picture
+	    
+	    With things run in parallel instead of series
+	        each picture in *.jpg *.png ++ do_something picture
